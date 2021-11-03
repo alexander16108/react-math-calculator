57bf05576344d32b2f0097bf27e57311236a5400
alexander16108 <alexandermayowa05@gmail.com>
2021-11-03 11:19:06 +0100

    Implementing change required for the button functionalities

--- src/component/calculator.js
+++ src/component/calculator.js
@@ -1,48 +1,70 @@
-import '../App.css';
+/* eslint-disable no-nested-ternary */
 import React from 'react';
+import calculate from '../logic/calculate';
+import '../logic/operate';
+import Buttons from './buttonEvents';
 
-class Calculator extends React.Component {
+export default class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      calcObj: {},
+    };
+    this.renderClickable = this.renderClickable.bind(this);
+  }
+
+  renderClickable(calcObj, text) {
+    this.setState({ calcObj: calculate(calcObj, text) });
   }
 
   render() {
+    const { calcObj } = this.state;
+    let contentDisplay = '0';
+    if (
+      Object.keys(calcObj).length === 0
+      || (calcObj.total === null
+        && calcObj.next === null
+        && calcObj.operation === null)
+    ) {
+      contentDisplay = '0';
+    } else {
+      contentDisplay = calcObj.next != null ? calcObj.next : calcObj.total;
+    }
+
     return (
       <div className="calc-container">
-        <div className="calc-firstDisplay">0</div>
+        <div className="calc-firstDisplay">{contentDisplay}</div>
         <div className="calc-Btns">
           <div className="calc-greyBtns">
             <div className="calc-greyBtn1">
-              <div className="calc-btn">AC</div>
-              <div className="calc-btn">+/-</div>
-              <div className="calc-btn">%</div>
-              <div className="calc-btn">7</div>
-              <div className="calc-btn">8</div>
-              <div className="calc-btn">9</div>
-              <div className="calc-btn">4</div>
-              <div className="calc-btn">5</div>
-              <div className="calc-btn">6</div>
-              <div className="calc-btn">1</div>
-              <div className="calc-btn">2</div>
-              <div className="calc-btn">3</div>
+              <Buttons text="AC" Obj={calcObj} onClick={this.renderClickable} />
+              <Buttons text="+/-" Obj={calcObj} onClick={this.renderClickable} />
+              <Buttons text="%" Obj={calcObj} onClick={this.renderClickable} />
+              <Buttons text="7" Obj={calcObj} onClick={this.renderClickable} />
+              <Buttons text="8" Obj={calcObj} onClick={this.renderClickable} />
+              <Buttons text="9" Obj={calcObj} onClick={this.renderClickable} />
+              <Buttons text="4" Obj={calcObj} onClick={this.renderClickable} />
+              <Buttons text="5" Obj={calcObj} onClick={this.renderClickable} />
+              <Buttons text="6" Obj={calcObj} onClick={this.renderClickable} />
+              <Buttons text="1" Obj={calcObj} onClick={this.renderClickable} />
+              <Buttons text="2" Obj={calcObj} onClick={this.renderClickable} />
+              <Buttons text="3" Obj={calcObj} onClick={this.renderClickable} />
             </div>
             <div className="calc-greyBtn2">
-              <div className="calc-btn1">0</div>
-              <div className="calc-btn2">.</div>
+              <Buttons text="0" Obj={calcObj} onClick={this.renderClickable} />
+              <Buttons text="." Obj={calcObj} onClick={this.renderClickable} />
             </div>
           </div>
 
           <div className="orange-btn">
-            <div className="calc-btn">/</div>
-            <div className="calc-btn">*</div>
-            <div className="calc-btn">-</div>
-            <div className="calc-btn">+</div>
-            <div className="calc-btn">=</div>
+            <Buttons text="/" Obj={calcObj} onClick={this.renderClickable} />
+            <Buttons text="*" Obj={calcObj} onClick={this.renderClickable} />
+            <Buttons text="-" Obj={calcObj} onClick={this.renderClickable} />
+            <Buttons text="+" Obj={calcObj} onClick={this.renderClickable} />
+            <Buttons text="=" Obj={calcObj} onClick={this.renderClickable} />
           </div>
         </div>
       </div>
     );
   }
 }
-export default Calculator;

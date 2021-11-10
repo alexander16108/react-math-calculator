fcc7998456b488d56883921a9f67c2935110de9e
alexander16108 <alexandermayowa05@gmail.com>
2021-11-10 16:49:48 +0100

    Adding other Location in the dom to display

--- src/component/pages/Quotes.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const Quote = () => (
-  <div>
-    <h3>No match for this page</h3>
-  </div>
-);
-export default Quote;
---
+++ src/pages/Quotes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Quote = () => (
+  <div className="quote-page">
+    <h1>today&apos;s quote....</h1>
+    <p>
+      When the seas of life get rough, remember Someone else was here in the
+      same boat with the same scenerio and scaled through. - Unknown
+    </p>
+  </div>
+);
+
+export default Quote;

9ae8cc7e4c0882ad63d582ad2f8d83bb04b7a2d2
alexander16108 <alexandermayowa05@gmail.com>
2021-11-11 11:54:46 +0100

    writing test for rendered pages

--- src/pages/Header.test.js
+++ src/pages/Header.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { BroswerRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './Header';
 
 it('renders correctly', () => {
